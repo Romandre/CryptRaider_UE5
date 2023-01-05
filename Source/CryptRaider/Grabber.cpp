a0868106a09d11bcb02d89a89c7076668af03883
Roman Kucherenko <roma_542@mail.ru>
2023-01-05 20:00:47 +0200

    Push project to repository

---
+++ Source/CryptRaider/Grabber.cpp
@@ -0,0 +1,106 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Grabber.h"
+
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	if (PhysicsHandle == nullptr) 
+	{
+		return;
+	}
+
+	FVector TargetLocation = GetComponentLocation() + GetForwardVector() * HoldDistance;
+	PhysicsHandle->SetTargetLocationAndRotation(TargetLocation, GetComponentRotation());
+}
+
+void UGrabber::Grab()
+{
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	if (PhysicsHandle == nullptr) 
+	{
+		return;
+	}
+
+	FVector Start = GetComponentLocation();
+	FVector End = Start + GetForwardVector() * MaxGrabDistance;
+	if (GrabberDebug)
+	{
+		DrawDebugSphere(GetWorld(), End, 10, 10, FColor::Blue, false, 5);
+	}
+	
+	FCollisionShape Sphere = FCollisionShape::MakeSphere(GrabRadius);
+	FHitResult HitResult;
+	bool HasHit = GetWorld()->SweepSingleByChannel(
+		HitResult, 
+		Start, 
+		End,
+		FQuat::Identity,
+		ECC_GameTraceChannel2,
+		Sphere
+	);
+
+	if (HasHit)
+	{
+		PhysicsHandle->GrabComponentAtLocationWithRotation(
+			HitResult.GetComponent(),
+			NAME_None,
+			HitResult.ImpactPoint,
+			GetComponentRotation()
+		);
+		
+		if (GrabberDebug)
+		{
+			DrawDebugSphere(GetWorld(), HitResult.Location, 10, 10, FColor::Green, false, 5);		
+			DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10, 10, FColor::Red, false, 5);
+			AActor* HitActor = HitResult.GetActor();
+			UE_LOG(LogTemp, Display, TEXT("Actor hit is: %s"), *HitActor->GetActorNameOrLabel());
+		}
+	} else if (GrabberDebug)
+	{
+		UE_LOG(LogTemp, Display, TEXT("No actor hit."));
+	}
+}
+
+void UGrabber::Release()
+{
+	if (GrabberDebug)
+	{
+		UE_LOG(LogTemp, Display, TEXT("Released grabber."));
+	}
+}
+
+UPhysicsHandleComponent* UGrabber::GetPhysicsHandle() const
+{
+	UPhysicsHandleComponent* Result = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (Result == nullptr)
+	{
+		UE_LOG(LogTemp, Display, TEXT("Grabber requires a UPhysicsHandleComponent."));
+	}
+	return Result;
+}
